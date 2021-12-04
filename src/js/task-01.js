e958d180a1d7cb9e1834fb17c4eafefdaf4adffe
Vira-Lishchuk <veronicanewlife25@gmail.com>
2021-12-04 14:39:41 +0200

    task1-2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,12 @@
-nbmnvv
+const listItems = document.querySelectorAll('li.item');
+
+console.log(`Number of categories : ${listItems.length}`);
+
+const ul = document.querySelectorAll('#categories > li');
+ul.forEach(element => {
+
+  console.log(
+      `Category: ${element.firstElementChild.textContent}
+ Elements: ${element.lastElementChild.children.length}`,
+  );
+});
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listItem = document.querySelector('#ingredients');
+const ingredientsElement = ingredients.map(ingredient => {
+  const listItemElement = document.createElement('li');
+  listItemElement.textContent = ingredient;
+
+  return listItemElement;
+});
+listItem.append(...ingredientsElement);
+console.log(listItem);
